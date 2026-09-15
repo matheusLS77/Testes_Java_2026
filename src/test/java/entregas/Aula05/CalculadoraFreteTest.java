634bed244123398a34ae696b233746554a5388a0
matheus_silva159 <matheusblu16@gmail.com>
2026-09-15 19:45:37 -0300

    adiciona comentarios

--- src/test/java/entregas/Aula05/CalculadoraFreteTest.java
+++ src/test/java/entregas/Aula05/CalculadoraFreteTest.java
@@ -9,7 +9,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class CalculadoraFreteTest {
     @DisplayName("Deve calcular o frete corretamente para diferentes pesos e tipos de frete")
-    //Define valores de teste para
+    //Define valores recebidos
     @ParameterizedTest(name = "peso={0}, expressa={1}, esperado={2}")
     // Dados fornecidos
     @CsvSource({
@@ -20,14 +20,16 @@ class CalculadoraFreteTest {
             "1.00, true,  15.00",
             "5.00, true,  27.00"
     })
-    void calcularDeveRetornarFreteCorreto(double peso, boolean expressa, double esperado) {
+    void calcularFreteCorreto(double peso, boolean expressa, double esperado) {
         double obtido = CalculadoraFrete.calcular(peso, expressa);
 
+        // Verifica se o valor obtido é igual ao valor esperado
         assertEquals(esperado, obtido, 0.001);
     }
 
     @DisplayName("Deve lançar exceção para peso inválido")
     @ParameterizedTest(name = "peso inválido: {0}")
+    // Lista de pesos inválidos para teste
     @ValueSource(doubles = {0.0, -0.01, -10.0})
     void pesoInvalidoDeveLancarExcecao(double peso) {
         // Verifica se a exceção é lançada para pesos inválidos
